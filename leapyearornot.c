8db43b766e8869a53485522f5808c8ae6aa33665
durgesh253 <110898816+durgesh253@users.noreply.github.com>
2022-08-27 12:55:41 +0530

    Update leapyearornot.c

--- leapyearornot.c
+++ leapyearornot.c
@@ -5,7 +5,7 @@ void main()
      printf("enter the year:");
      scanf("%d",&year);
 
-     if (year%4==0)
+     if (year%4==0 && year%100!=0)
      {
         printf("%d is leap year:",year);
      }
